5b88c707622fcaea1564f672b050261374c1a978
Gil Desmarais <gil@desmarais.de>
2015-06-07 19:40:06 +0200

    Create/Update factories

---
+++ spec/factories/region_slugs.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :region_slug do
+    slug "MyString"
+    region nil
+  end
+end
--- spec/support/factories/region.rb
+++ spec/support/factories/region.rb
@@ -3,19 +3,19 @@ FactoryGirl.define do
   factory :global_region, class: 'region' do
     id 1
     name "global"
-    slug "global"
+    region_slug { RegionSlug.find_by_slug("global") || FactoryGirl.create(:region_slug, region_id: 1, slug: "global") }
   end
 
   factory :koeln_region, class: 'region' do
     id 2
     name "Köln"
-    slug "koeln"
+    region_slug { RegionSlug.find_by_slug("koeln") || FactoryGirl.create(:region_slug, region_id: 2, slug: "koeln") }
   end
 
   factory :berlin_region, class: 'region' do
     id 3
     name "Berlin"
-    slug "berlin"
+    region_slug { RegionSlug.find_by_slug("berlin") || FactoryGirl.create(:region_slug, region_id: 3, slug: "berlin") }
   end
 end
 
